addc17744a6a7ac249da8bd80ee4c6c8710cae52
Miłosz Panas <miloszpanas@gmail.com>
2018-06-21 14:48:18 +0200

    Add independent button components

--- script.js
+++ script.js
@@ -13,10 +13,42 @@ var Counter = React.createClass({
 
     render: function() {
         return React.createElement('div', {onClick: this.increment},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+                React.createElement('button', {}, 'Licznik ' + this.state.counter),
+        )
+    }
+});
+
+var CounterMinus = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {onClick: this.decrement}, 
+                React.createElement('button', {}, 'Licznik ' + this.state.counter),
+        )
+    }
+});
+
+
+var counterApp = React.createClass({
+    render: function() {
+        return (
+            React.createElement('div', {className: 'counterWrapper'}, 
+                React.createElement(Counter),
+                React.createElement(CounterMinus)
+            )
         );
     }
 });
 
-var element = React.createElement(Counter);
+var element = React.createElement(counterApp);
 ReactDOM.render(element, document.getElementById('app'));
