3bcaf0fc174b7f36dde184d3d7ce159b3049df82
Miłosz Panas <miloszpanas@gmail.com>
2018-06-22 14:24:31 +0200

    Add completed task

--- script.js
+++ script.js
@@ -18,6 +18,38 @@ var Counter = React.createClass({
         });
     },
 
+    getDefaultProps: function() {
+        return console.log('ustaw domyślne właściwości na obiekt z jakimiś danymi');
+    },
+
+    componentWillMount: function() {
+        return console.log('komponent zostanie zamontowany po czym coś tam się stanie, nie mam pomysłu');
+    },
+
+    componentDidMount: function() {
+        return console.log('komponent wywołany po momencie wywołania metody render, widnieje na stronie w drzewie DOM, na razie brak pomysłu na wykorzystanie');
+    },
+
+    componentWillReceiveProps: function() {
+        return console.log('przekazanie komponentowi nowych propów, dokumentacja reacta wskazuje, że używanie go nie jest zalecane i zostanie wkrótce zastąpione');
+    },
+
+    shouldComponentUpdate: function() {
+        return console.log('metoda wywoływana w momencie, gdy zwróci wartość true i zaisntnieje potrzeba zaktualizowania komponentu, co by to miało być, nie ma pomysłu');
+    },
+
+    componentWillUpdate: function() {
+        return console.log('zaktualizowanie komponentu');
+    },
+
+    componentDidUpdate: function() {
+        return console.log('metoda wywoływana od razu w momencie aktualizacji');
+    },
+
+    componentWIllUnmount: function() {
+        return console.log('metoda wywoływana bezpośrednio przed odmontowaniem komponentu, służy do czyszczenia, inwalidacji liczników, itp, realnego pomysłu brak');
+    },
+
     render: function() {
         return React.createElement('div', {},
                 React.createElement('button', {onClick: this.decrement}, 'Licznik ' + this.state.counter2),
