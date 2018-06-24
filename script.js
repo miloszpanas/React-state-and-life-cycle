1eb32f3bca7382b41ea34296715eb4b76799061c
Miłosz Panas <miloszpanas@gmail.com>
2018-06-24 10:06:15 +0200

    Add proper counters

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var Counter = React.createClass({
     getInitialState: function() {
         return {
             counter: 0,
-            counter2: 0
+            // counter2: 0
         };
     },
 
@@ -14,51 +14,58 @@ var Counter = React.createClass({
 
     decrement: function() {
         this.setState({
-            counter2: this.state.counter2 - 1
+            counter: this.state.counter - 1
         });
     },
 
     getDefaultProps: function() {
-        return console.log('ustaw domyślne właściwości na obiekt z jakimiś danymi');
+         console.log('ustaw domyślne właściwości na obiekt z jakimiś danymi');
     },
 
     componentWillMount: function() {
-        return console.log('komponent zostanie zamontowany po czym coś tam się stanie, nie mam pomysłu');
+         console.log('komponent zostanie zamontowany po czym coś tam się stanie, nie mam pomysłu');
     },
 
     componentDidMount: function() {
-        return console.log('komponent wywołany po momencie wywołania metody render, widnieje na stronie w drzewie DOM, na razie brak pomysłu na wykorzystanie');
+         console.log('komponent wywołany po momencie wywołania metody render, widnieje na stronie w drzewie DOM, na razie brak pomysłu na wykorzystanie');
     },
 
     componentWillReceiveProps: function() {
-        return console.log('przekazanie komponentowi nowych propów, dokumentacja reacta wskazuje, że używanie go nie jest zalecane i zostanie wkrótce zastąpione');
+         console.log('przekazanie komponentowi nowych propów, dokumentacja reacta wskazuje, że używanie go nie jest zalecane i zostanie wkrótce zastąpione');
     },
 
     shouldComponentUpdate: function() {
-        return console.log('metoda wywoływana w momencie, gdy zwróci wartość true i zaisntnieje potrzeba zaktualizowania komponentu, co by to miało być, nie ma pomysłu');
+        console.log('metoda wywoływana w momencie, gdy zwróci wartość true i zaisntnieje potrzeba zaktualizowania komponentu, co by to miało być, nie ma pomysłu');
+        return true;
     },
 
     componentWillUpdate: function() {
-        return console.log('zaktualizowanie komponentu');
+         console.log('zaktualizowanie komponentu');
     },
 
     componentDidUpdate: function() {
-        return console.log('metoda wywoływana od razu w momencie aktualizacji');
+         console.log('metoda wywoływana od razu w momencie aktualizacji');
     },
 
     componentWIllUnmount: function() {
-        return console.log('metoda wywoływana bezpośrednio przed odmontowaniem komponentu, służy do czyszczenia, inwalidacji liczników, itp, realnego pomysłu brak');
+         console.log('metoda wywoływana bezpośrednio przed odmontowaniem komponentu, służy do czyszczenia, inwalidacji liczników, itp, realnego pomysłu brak');
     },
 
     render: function() {
         return React.createElement('div', {},
-                React.createElement('button', {onClick: this.decrement}, 'Licznik ' + this.state.counter2),
-                React.createElement('button', {onClick: this.increment}, 'Licznik ' + this.state.counter),
+                React.createElement('button', {onClick: this.decrement}, '-'),
+                React.createElement('span', {}, this.state.counter),
+                React.createElement('button', {onClick: this.increment}, '+'),
         )
     }
 });
 
-var element = React.createElement(Counter);
+var element = React.createElement('div', {}, 
+    React.createElement(Counter), 
+    React.createElement(Counter), 
+    React.createElement(Counter)
+);
+
 ReactDOM.render(element, document.getElementById('app'));
 
 // var CounterMinus = React.createClass({
